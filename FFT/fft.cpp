9039549256fde3e67394e9b9ec57781982531488
Bailey Martin <Ms.BaileyL.Martin@gmail.com>
2024-02-25 16:03:48 -0700

    Splitting by even and odd indices correctly

--- FFT/fft.cpp
+++ FFT/fft.cpp
@@ -6,15 +6,35 @@
 typedef std::complex<double> complex_num;
 typedef std::vector<complex_num> vec_complex_num;
 
-std::tuple<vec_complex_num, vec_complex_num> split_evens_and_odds(vec_complex_num)
+std::tuple<vec_complex_num, vec_complex_num> split_evens_and_odds(vec_complex_num original_vec)
 {
-    return std::make_tuple(vec_complex_num{1.0}, vec_complex_num{2.0});
+    size_t size_vec = original_vec.size();
+    if(size_vec % 2 != 0)
+    {
+        std::cout << "Sample size should be an even number" << std::endl;
+        std::terminate();
+    }
+
+    
+    vec_complex_num evens = vec_complex_num(size_vec/2);
+    vec_complex_num odds = vec_complex_num(size_vec/2);
+
+    size_t i;
+    size_t j;
+    for(i = 0, j = 0; i < size_vec; i+=2, j++)
+    {
+        std::cout << "i is: " << i << " j is: " << j << std::endl;
+        evens[j] = original_vec[i]; 
+        odds[j] = original_vec[i+1]; 
+    }
+
+    return std::make_tuple(evens, odds);
 }
 
 int main ()
 {
     // Get the tuple of vectors from the function
-    auto result = split_evens_and_odds({1.0, 2.0});
+    auto result = split_evens_and_odds({1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0});
 
     // Extract the vectors from the tuple
     vec_complex_num evens = std::get<0>(result);
