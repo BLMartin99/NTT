ef88bd58ee8bc15884db58b5babd3dc9b9095535
Bailey Martin <Ms.BaileyL.Martin@gmail.com>
2024-03-03 13:49:20 -0700

    No compiler errors

--- FFT/fft_radix-2_iter/fft_radix-2_iter.cpp
+++ FFT/fft_radix-2_iter/fft_radix-2_iter.cpp
@@ -8,47 +8,10 @@ typedef std::complex<double> complex_num;
 const complex_num i (0.0, 1.0); // imaginary number
 typedef std::vector<complex_num> vec_complex_num;
 
-std::tuple<vec_complex_num, vec_complex_num> split_evens_and_odds(vec_complex_num original_vec)
-{
-    size_t size_vec = original_vec.size();
-    if(size_vec % 2 != 0)
-    {
-        std::cout << "Sample size should be an even number" << std::endl;
-        std::terminate();
-    }
-
-    
-    vec_complex_num evens = vec_complex_num(size_vec/2);
-    vec_complex_num odds = vec_complex_num(size_vec/2);
-
-    size_t k;
-    size_t j;
-    for(k = 0, j = 0; k < size_vec; k+=2, j++)
-    {
-        evens[j] = original_vec[k]; 
-        odds[j] = original_vec[k+1]; 
-    }
-
-    return std::make_tuple(evens, odds);
-}
-
-complex_num compute_twiddle_factor(size_t k, size_t N)
+complex_num compute_twiddle_factor(size_t m)
 { 
     complex_num pi (M_PI, 0.0);
-    return std::exp((complex_num(-2.0, 0.0)*pi*i*complex_num((double)k, 0.0))/complex_num((double)N, 0.0));
-}
-
-vec_complex_num get_twiddle_factors(size_t N)
-{
-    vec_complex_num vec_twiddle_factors(N);
-
-    size_t k;
-    for(k = 0; k < N/2; k++)
-    {
-       vec_twiddle_factors[k] = compute_twiddle_factor(k, N);
-    }
-
-    return vec_twiddle_factors;
+    return std::exp((complex_num(-2.0, 0.0)*pi*i, 0.0)/complex_num((double)m, 0.0));
 }
 
 uint32_t bit_reversal(uint32_t num, size_t N)
@@ -72,9 +35,9 @@ uint32_t bit_reversal(uint32_t num, size_t N)
     return reverse;
 }
 
-vec_complex_num vec_bit_reversal(vec_complex_num x)
+vec_complex_num vec_bit_reversal(vec_complex_num a)
 {
-    size_t N = x.size();
+    size_t N = a.size();
 
     if(N % 2 != 0)
     {
@@ -84,23 +47,55 @@ vec_complex_num vec_bit_reversal(vec_complex_num x)
 
     vec_complex_num vec_reversed(N);
     uint32_t reversed_index;
-    size_t j;
-    for (j = 0; j < N; j++)
+    size_t k;
+    for (k = 0; k < N; k++)
     {
-       reversed_index = bit_reversal(j, N);
-       vec_reversed[reversed_index] = x[j]; 
+       reversed_index = bit_reversal(k, N);
+       vec_reversed[reversed_index] = a[k]; 
     }
 
     return vec_reversed;
 }
 
-/*
-void fft (vec_complex_num &x)
+vec_complex_num fft (vec_complex_num a)
 {
-    
-}
-*/
+    // Get size N
+    size_t N = a.size();
 
+    // Log2(N)
+    size_t log2N = log2(N);
+
+    // Reverse bit LUT
+    vec_complex_num A = vec_bit_reversal(a);
+    
+    // FFT loop
+    size_t s;
+    size_t k;
+    size_t j;
+    size_t m;
+    complex_num w_m;
+    complex_num w;
+    complex_num t;
+    complex_num u;
+    for(s = 1; s <= log2N; s++)
+    {
+      m = (size_t)std::pow(2.0,(double)s); 
+      w_m = compute_twiddle_factor(m);
+       for(k = 0; k < N; k+=m)
+       {
+          w = 1;
+          for(j = 0; j < m/2; j++)
+          {
+            t = w*A[k + j + m/2];
+            u = A[k + j];
+            A[k + j] = u + t;
+            A[k + j + m/2] = u - t;
+            w = w*w_m;
+          }
+       }
+    }
+    return A;
+}
 
 int main ()
 {
@@ -119,8 +114,6 @@ int main ()
     {
         std::cout << num.real() << " + " << num.imag() << "i" << std::endl;
     }
-
-
 
     /*
     std::cout << "Input vector" << std::endl;
