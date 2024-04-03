be167189024c14992baf73cba355fb5098c468f6
Bailey Martin <Ms.BaileyL.Martin@gmail.com>
2024-04-02 18:15:26 -0600

    Need to rethink how do vec_bit_reversal

--- NTT/verilog/bit_reversal.v
+++ NTT/verilog/bit_reversal.v
@@ -2,8 +2,6 @@ module bit_reversal (num, reverse);
     input wire [2:0] num;
     output wire [2:0] reverse;
 
-    assign reverse[0] = num[2]; 
-    assign reverse[1] = num[1]; 
-    assign reverse[2] = num[0]; 
+    assign reverse = {num[0], num[1], num[2]}; 
 
 endmodule
